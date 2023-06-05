cbdcef2446cc1b297956fa79d122b0d26eb2a65e
Hxrsh <58227294+Hxrsh@users.noreply.github.com>
2023-06-05 23:32:09 +0530

    pagination logic built
    fixed some other minor pagination related flows

--- src/RecipeList/RecipeList.js
+++ src/RecipeList/RecipeList.js
@@ -7,7 +7,11 @@ const RecipeList = (props) => {
   console.log("rendered RecipeList");
   const [recipeListData, setRecipeListData] = useState("");
   const { onSearchValRec: SearchedVal } = props;
-
+  const [paginationDisp, setPaginationDisp] = useState({ start: 0, end: 9 });
+  const recipeListDisplayed = recipeListData.slice(
+    paginationDisp.start,
+    paginationDisp.end
+  );
   useEffect(() => {
     fetchRecipeList(SearchedVal, setRecipeListData);
   }, [SearchedVal]);
@@ -18,25 +22,26 @@ const RecipeList = (props) => {
     <div className="search__results list">
       <ul className="recipe_list">
         {recipeListData &&
-          recipeListData?.map((el, index) => {
-            while (index < 10) {
-              return (
-                <li className="recipe" key={index}>
-                  <a className="recipe_link" href={`${"#"}${el.id}`}>
-                    <div className="recipe_img_wrap">
-                      <img src={el.image_url} className="list_image" />
-                    </div>
-                    <div className="list_recipe_wrap">
-                      <span className="list_title">{el.title}</span>
-                      <span className="list_publisher">{el.publisher}</span>
-                    </div>
-                  </a>
-                </li>
-              );
-            }
+          recipeListDisplayed?.map((el, index) => {
+            return (
+              <li className="recipe" key={index}>
+                <a className="recipe_link" href={`${"#"}${el.id}`}>
+                  <div className="recipe_img_wrap">
+                    <img src={el.image_url} className="list_image" />
+                  </div>
+                  <div className="list_recipe_wrap">
+                    <span className="list_title">{el.title}</span>
+                    <span className="list_publisher">{el.publisher}</span>
+                  </div>
+                </a>
+              </li>
+            );
           })}
       </ul>
-      <Pagination></Pagination>
+      <Pagination
+        onListLoadDataLength={recipeListData.length}
+        onPaginationClicked={setPaginationDisp}
+      ></Pagination>
     </div>
   );
 };
--- src/RecipeList/pagination/Pagination.js
+++ src/RecipeList/pagination/Pagination.js
@@ -1,26 +1,49 @@
 import "./Pagination.css";
 import pag_r from "../../img/right-arrow-pag.svg";
 import pag_l from "../../img/left-arrow-pag1.svg";
+import { useEffect, useState } from "react";
 const Pagination = (props) => {
-  const paginationLogic = (page) => {
-    const start = (page - 1) * 10;
-    const end = page * 10;
-    return [start, end];
+  const [currPage, setCurrPage] = useState(1);
+  const itemsDisplayed = 9;
+  const totalPages = Math.ceil(props.onListLoadDataLength / itemsDisplayed);
+  let prevBtnStyle = "pag_btn";
+  let nextBtnStyle = "pag_btn";
+
+  //PAgination button hiding logic
+  if (currPage === 1) {
+    prevBtnStyle = " pag_hide";
+  } else if (currPage === totalPages) {
+    nextBtnStyle = " pag_hide";
+  }
+
+  const pagLogic = (currPage) => {
+    const start = (currPage - 1) * itemsDisplayed;
+    const end = currPage * itemsDisplayed;
+    return { start: start, end: end };
   };
-  const onPrevPageHandler = (page) => {
-    if (page > 0) return;
-  };
-  const onNextPageHandler = (page) => {
-    if (page > 0) return;
-  };
+
+  useEffect(() => {
+    props.onPaginationClicked(pagLogic(currPage));
+  }, [currPage]);
+
   return (
     <div className="pagination_wrap">
-      <button className="pag_btn " onClick={onPrevPageHandler}>
+      <button
+        className={prevBtnStyle}
+        onClick={() => {
+          setCurrPage(currPage - 1);
+        }}
+      >
         <img src={pag_l} className="pagination_arr " />
-        <span>Page 1</span>
+        <span>Page {currPage - 1}</span>
       </button>
-      <button className="pag_btn" onClick={onNextPageHandler}>
-        <span>Page 2</span>
+      <button
+        className={nextBtnStyle}
+        onClick={() => {
+          setCurrPage(currPage + 1);
+        }}
+      >
+        <span>Page {currPage + 1}</span>
         <img src={pag_r} className="pagination_arr" />
       </button>
     </div>
